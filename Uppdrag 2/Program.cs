ea08a162dd8bb10b429b7db2957e6ed8c5aaf315
Buecho <112667017+Buecho@users.noreply.github.com>
2023-11-12 23:26:40 +0100

    Add files via upload

---
+++ Uppdrag 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 65;
+            Console.WriteLine("vad heter du");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+
+            Console.WriteLine("Hur gammal är du");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(age);
+
+            Console.WriteLine("du tar pensionär om" + (x-age) + "år");
+                
+        }
+    }
+}
---
+++ Uppdrag 2/uppdrag 2, pension - python.py
@@ -0,0 +1,13 @@
+# Namn
+a = input("Vad heter du:")
+print(a)
+
+#Berättar hur gammal personen är
+b = input("Hur gammal är du:")
+b = int(b)
+
+# Pensionåldern 
+c = 65
+
+#slutsats
+print("Hej", str(a),"du går i pension om", str(c-b), "år")
